298490c4746c4ea904558ab491916b4592368979
RixXuan <ritchie.xuan@gmail.com>
2025-02-25 18:01:51 +0100

    fix init

--- utils copy/__init__.py
+++ utils copy/__init__.py
@@ -1,2 +1,1 @@
 from .cli import CommandLineInterface
-from .prompts import *
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,8 +1,2 @@
-"""
-Discord Mass DM Tool
-
-A modular Python-based tool for sending direct messages to multiple Discord users.
-"""
-
-__version__ = "1.0.0"
-__author__ = "Developer"
+from .helpers import *
+from .logger import *
